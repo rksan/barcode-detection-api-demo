ef4e7ea5e8586b7819dbc049c368f97c1e811480
rk <48819681+rksan@users.noreply.github.com>
2023-02-15 13:32:13 +0900

    installed bootstrap family

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-createApp(App).mount("#app");
+import BootstrapVueNext from "bootstrap-vue-next";
+
+// Optional, since every component imports their Bootstrap functionality
+// the following line is not necessary
+// import 'bootstrap'
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+createApp(App).use(BootstrapVueNext).mount("#app");
