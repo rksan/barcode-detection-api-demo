ded669163fd75305febd5fde3940e8e82ae4e783
rk <48819681+rksan@users.noreply.github.com>
2023-02-15 13:22:46 +0900

    add /docs

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
+const webpack = require("webpack");
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  // github pages
+  outputDir: "docs",
+  assetsDir: "./",
+  publicPath: "./",
   transpileDependencies: true,
+  configureWebpack: {
+    plugins: [
+      require("unplugin-vue-components/webpack")({
+        /* options */
+      }),
+    ],
+  },
+  chainWebpack: (config) =>
+    config.plugin("feature-flags").use(webpack.DefinePlugin, [
+      {
+        __VUE_PROD_DEVTOOLS__: JSON.stringify(true),
+      },
+    ]),
 });
