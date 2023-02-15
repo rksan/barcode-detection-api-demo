f4cba839068053a66fe9d4d699f516d51e073e2e
rk <48819681+rksan@users.noreply.github.com>
2023-02-15 15:48:15 +0900

    add Quagga

--- src/components/pages/PInputForm.vue
+++ src/components/pages/PInputForm.vue
@@ -1,73 +1,64 @@
 <template>
-  <div v-if="!checkBrowser">
-    <p>Barcode Detector はこのブラウザーでは対応していません。</p>
-    <p>
-      <b-link
-        href="https://developer.mozilla.org/ja/docs/Web/API/Barcode_Detection_API#%E3%83%96%E3%83%A9%E3%82%A6%E3%82%B6%E3%83%BC%E3%81%AE%E4%BA%92%E6%8F%9B%E6%80%A7"
-        target="_blank"
-      >
-        Barcode Detection API | MDN Web Docs
-        <i class="bi bi-box-arrow-up-right"></i>
-      </b-link>
-    </p>
-    <p>UserAgent : {{ userAgent }}</p>
-  </div>
-  <div v-else>
-    <div class="row">
-      <b-button @click="doStart">
-        <i class="bi bi-upc-scan"></i> Barcode
-      </b-button>
-    </div>
-    <div class="row" id="my-console">{{ model.text }}</div>
-  </div>
+  <p>Start をクリック後、ブラウザにカメラの使用を許可してください。</p>
+
+  <div id="interactive" class="viewport"></div>
+
+  <b-button @click="doStart" variant="primary">
+    <i class="bi bi-upc-scan"></i> Start
+  </b-button>
+
+  <b-button @click="doStop" variant="danger">
+    <i class="bi bi-stop-circle-fill"></i> Stop
+  </b-button>
+
+  <p>{{ model.log }}</p>
 </template>
 
 <script>
-import BarcodeDetector from "barcode-detector";
-
-// polyfill unless already supported
-if (!("BarcodeDetector" in window)) {
-  console.warn(
-    "[Warn] use Polyfill BardoceDetecotr. @ref : https://www.npmjs.com/package/barcode-detector"
-  );
-  //window.BarcodeDetector = BarcodeDetector;
-}
+import Quagga from "quagga";
 
 export default {
   name: "pInputForm",
 
   data() {
     return {
-      model: { text: "" },
+      model: {
+        log: "",
+      },
     };
   },
 
   setup() {},
 
-  computed: {
-    userAgent() {
-      return window.navigator.userAgent;
-    },
-    checkBrowser() {
-      return "BarcodeDetector" in window;
-    },
-  },
-
   methods: {
     console(msg) {
-      this.model.text = msg;
+      this.model.log = msg;
+    },
+    doStop() {
+      Quagga.stop();
     },
     doStart() {
-      // 新しい検出器を生成
-      /* var barcodeDetector = new BarcodeDetector({
-        formats: ["code_39", "codabar", "ean_13"],
-      }); */
-
-      // 対応している型をチェック
-      BarcodeDetector.getSupportedFormats().then((supportedFormats) => {
-        supportedFormats.forEach((format) => this.console(format));
+      Quagga.init({
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: document.querySelector("#interactive.viewport"),
+        },
+        function(err) {
+          if (err) {
+            console(err);
+            return;
+          }
+          console("Initialization finished. Ready to start");
+          Quagga.start();
+        },
       });
+
+      Quagga.onProcessed(this.doProcessed);
+      Quagga.onDetected(this.doDetected);
     },
+    doProcessed() {},
+    doDetected() {},
   },
 };
 </script>
